ac45427ede31c098f7f3b7c4e148a9ff2eede26d
BingoBoy479 <132748942+BingoBoy479@users.noreply.github.com>
2026-06-11 18:02:50 +0530

    Add files via upload

---
+++ compiler/include/tokenizer.h
@@ -0,0 +1,64 @@
+#pragma once
+#include <cstdint>
+#include <string>
+#include <vector>
+
+enum TokenType
+{
+    End,
+    Word,
+    Number,
+
+    KwStart,
+    KwFunction,
+    KwIf,
+    KwElse,
+    KwWhile,
+    KwFor,
+    KwReturn,
+    KwInt,
+
+    Plus,
+    Increase,
+    Minus,
+    Decrease,
+    Star,
+    Slash,
+    Mod,
+
+    Assign,
+    PlusAssign,
+    MinusAssign,
+    
+    AndAnd,
+    OrOr, 
+    EqualEqual,
+
+    Less,
+    LessEqual,
+
+    Greater,
+    GreaterEqual,
+
+    Not,
+    NotEqual,
+
+    LeftParen,
+    RightParen,
+
+    LeftBrace,
+    RightBrace,
+    
+    UnaryMinus,
+    Comma,
+    Semicolon
+};
+
+struct Token
+{
+    TokenType name;
+    uint32_t start;
+    uint32_t length;
+};
+std::vector<Token> tokenize(const std::string& source);
+void printTokens(std::vector<Token>& file_tokens,const std::string& source);
